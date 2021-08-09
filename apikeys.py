97e733f65ceef7947e349bb90cbc583b55042d39
Philip Colllier <redcrown01@gmail.com>
2021-08-09 09:26:36 +0000

    Add files via upload

---
+++ apikeys.py
@@ -0,0 +1,15 @@
+#  API KEYS
+
+# For most APIs, you need to either define your keys
+# here or export them as environmental variables.
+# If defined here, write your scripts to import
+# the variables you need.  For example:
+#  from apikeys.py import reddit_client_id
+
+# reddit API authentication credentials
+reddit_client_id = '<---your-client-id--->'
+reddit_client_secret = '<---your-client-secret--->'
+reddit_user_agent = '<---your-user-agent--->'
+
+# google api key
+google_apikey = '<---your-google-api-key--->'
